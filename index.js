8af0c90b35b394ad66aab13b864561586935ea0f
root <root@Windows11.localdomain>
2021-09-24 12:48:47 -0300

    Inclusão da lista de veículos

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ server.use(express.json());
 
 //Dados Utilizados
 const users = [
-    {id:'1', placa: 'KMP-2130', chassi: '8Ax HCP9C3 ez 1y5661', renavam: '13766268391', marca: 'GM - Chevrolet', modelo: 'CRUZE LTZ 1.8 16V FlexPower 4p Aut.', ano: '2009'},  
-    {id:'2', placa: 'KRZ-9492', chassi: '6t3 j8A0cT 4H G10196', renavam: '97849938131', marca: 'Renault', modelo: 'Master 2.3 dCi Chassi 16V Diesel', ano: '2021'},
-    {id:'3', placa: 'KXT-0000', chassi: '1r2 tAA535 U7 Jb8421', renavam: '97882911328', marca: 'Ford', modelo: 'Fiesta 1.0 8V Flex/Class 1.0 8V Flex 5p', ano: '2012'},
-    {id:'4', placa: 'LQH-1513', chassi: '2A9 5aMACV 09 by3625', renavam: '02140582963', marca: 'Mitsubishi', modelo: 'Eclipse GS/ GS Turbo Mec', ano: 2008}
+    {veiculo:'Chevrolet Cruize', id:'1', placa: 'KMP-2130', chassi: '8Ax HCP9C3 ez 1y5661', renavam: '13766268391', marca: 'GM - Chevrolet', modelo: 'CRUZE LTZ 1.8 16V FlexPower 4p Aut.', ano: '2009'},  
+    {veiculo:'Renault Master',id:'2', placa: 'KRZ-9492', chassi: '6t3 j8A0cT 4H G10196', renavam: '97849938131', marca: 'Renault', modelo: 'Master 2.3 dCi Chassi 16V Diesel', ano: '2021'},
+    {veiculo:'Ford Fiesta', id:'3', placa: 'KXT-0000', chassi: '1r2 tAA535 U7 Jb8421', renavam: '97882911328', marca: 'Ford', modelo: 'Fiesta 1.0 8V Flex/Class 1.0 8V Flex 5p', ano: '2012'},
+    {veiculo:'Mitsubishi Eclipse', id:'4', placa: 'LQH-1513', chassi: '2A9 5aMACV 09 by3625', renavam: '02140582963', marca: 'Mitsubishi', modelo: 'Eclipse GS/ GS Turbo Mec', ano: 2008}
 ]
 
 
